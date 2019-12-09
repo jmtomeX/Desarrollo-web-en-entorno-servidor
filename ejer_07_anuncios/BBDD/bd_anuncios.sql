0c0fa59b399f8d425b3c494c0426ef8510508dca
JOSE M TOME MEJIAS <1920OPDW107@CEBANCDEA.EDU>
2019-12-09 17:13:09 +0100

    terminado ejemplo páginación2

--- ejer_07_anuncios/BBDD/bd_anuncios.sql
+++ ejer_07_anuncios/BBDD/bd_anuncios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2019 a las 19:30:52
+-- Tiempo de generación: 09-12-2019 a las 17:12:26
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.10
 
@@ -31,10 +31,44 @@ SET time_zone = "+00:00";
 CREATE TABLE `anuncios` (
   `an_id` int(11) NOT NULL,
   `an_titulo` varchar(40) NOT NULL,
-  `an_descripcion` varchar(200) NOT NULL,
+  `an_descripcion` varchar(200) DEFAULT NULL,
   `an_precio` float NOT NULL,
-  `an_foto` varchar(40) NOT NULL,
+  `an_foto` varchar(40) DEFAULT NULL,
+  `an_vistas` int(11) NOT NULL,
+  `an_visitas` int(11) NOT NULL,
   `an_us_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `anuncios`
+--
+
+INSERT INTO `anuncios` (`an_id`, `an_titulo`, `an_descripcion`, `an_precio`, `an_foto`, `an_vistas`, `an_visitas`, `an_us_id`) VALUES
+(1, 'Coche eléctrico', 'losdaoisdfañsdfoijn dsafjñ sa\r\n asdfhjsfoahfñ\r\nf sdañfji', 21000, '', 21, 2, 1),
+(6, 'gsarhoñ', 'ñsgilah', 534, '', 17, 0, 1),
+(10, 'sdfg', 'sdfg', 4444, '', 14, 7, 1),
+(15, 'sdfg', 'werw', 44, '', 14, 0, 1),
+(21, 'Bici Montaña', 'Cannondale Habit', 4999, 'ac3e6ded68cbcfdfe91381d491dcbe97.jpg', 21, 3, 1),
+(22, 'Bici carretera ', 'Orbea Orca', 2500, '68aed71dff6733a244297fd31c4b0396.jpg', 18, 12, 23),
+(23, 'Coche eléctrico', 'losdaoisdfañsdfoijn dsafjñ sa\r\n asdfhjsfoahfñ\r\nf sdañfji', 21000, '', 21, 2, 1),
+(24, 'gsarhoñ', 'ñsgilah', 534, '', 17, 0, 1),
+(25, 'sdfg', 'sdfg', 4444, '', 14, 7, 1),
+(26, 'sdfg', 'werw', 44, '', 14, 0, 1),
+(27, 'Bici Montaña', 'Cannondale Habit', 4999, 'ac3e6ded68cbcfdfe91381d491dcbe97.jpg', 21, 3, 1),
+(28, 'Bici carretera ', 'Orbea Orca', 2500, '68aed71dff6733a244297fd31c4b0396.jpg', 18, 12, 23);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `activation` varchar(300) NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -57,7 +91,12 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`us_id`, `us_name`, `us_email`, `us_password`) VALUES
 (1, 'jose mari', 'jm@jm.com', '1234'),
 (3, 'pp', 'pp@pp.com', '1234'),
-(4, 'v', 'v@v.com', 'v');
+(4, 'v', 'v@v.com', 'v'),
+(14, '234', 'pp@pp.co', '111111'),
+(15, 'gaw', 'pp@pp.co', '111111'),
+(21, 'sadf', 'iremti2@gmail.com', '111111'),
+(23, 'jm', '2@gmail.com', '111111'),
+(24, 'yw554546', 'yww', '111111');
 
 --
 -- Índices para tablas volcadas
@@ -71,6 +110,14 @@ ALTER TABLE `anuncios`
   ADD KEY `an_us_id` (`an_us_id`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `activation` (`activation`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -82,10 +129,22 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `anuncios`
+--
+ALTER TABLE `anuncios`
+  MODIFY `an_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `us_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `us_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Restricciones para tablas volcadas
